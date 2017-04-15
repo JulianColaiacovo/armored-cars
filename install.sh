1312da1a859c0c5661d5ec829e001aa1a515f900
julian <julian.colaiacovo@gmail.com>
2017-04-15 00:22:39 -0300

    install

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_6.x
+sudo apt-get -y install nodejs
 sudo apt-get -y install openjdk-8-jdk
 sudo apt-get -y install maven
 
+mkdir ${HOME}/npm-global
 npm config set prefix ${HOME}/npm-global
 
 mvn clean install
