720ddef58aebce2bad394d82d6542995801d4dd3
julian <julian.colaiacovo@gmail.com>
2017-04-15 00:50:51 -0300

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
-sudo apt-get -y install nodejs
+sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install npm
 sudo apt-get -y install openjdk-8-jdk
 sudo apt-get -y install maven
 
 mkdir ${HOME}/npm-global
 npm config set prefix ${HOME}/npm-global
+export PATH="${HOME}/npm-global/bin:$PATH"
 
 mvn clean install
