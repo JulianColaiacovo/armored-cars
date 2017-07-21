08eada272e7768f66ff6a83da3c37e6adbd44031
julian <julian.colaiacovo@gmail.com>
2017-07-21 02:09:10 -0300

    fix error al borrar una factura

--- armored-cars-service/src/main/webapp/app/formatters/document_formatter.js
+++ armored-cars-service/src/main/webapp/app/formatters/document_formatter.js
@@ -1,7 +1,7 @@
 App.filter("DocumentFormatter", function () {
     return function (document, documentType) {
         if (documentType === 'CUIL' || documentType === 'CUIT') {
-            return document.replace(/(\d{2})(\d{7,8})(\d)/, '$1-$2-$3')
+            return document.replace(/(\d{2})(\d{7,8})(\d)/, '$1-$2-$3');
         }
         return document;
     }
